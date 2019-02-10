34c04f6caa07ce04f6a7ddbb4b3bbcc81bccf2e9
Jieqiong Yu <jieqiong-yu@mercari.com>
2019-02-11 00:42:37 +0900

    Add gRPC Unary examples

--- src/greet/greet_client/client.go
+++ src/greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -12,7 +13,7 @@ func main() {
 	fmt.Println("Hello I'm a client")
 
 	// by default gRPC has SSL, for now, without this
-	conn, err := grpc.Dial("localhost:500051", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
@@ -22,5 +23,22 @@ func main() {
 
 	// create a new client
 	c := greetpb.NewGreetServiceClient(conn)
-	fmt.Printf("Created client: %f", c)
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Jieqiong",
+			LastName:  "Yu",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+
+	log.Printf("Response from Greet: %v", res.Result)
 }
--- src/greet/greet_server/server.go
+++ src/greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -10,6 +11,18 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v", req)
+	// extract information from request
+	firstName := req.GetGreeting().GetFirstName()
+	// form a response
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
 
 func main() {
 	fmt.Println("Hello, I'm a server")
